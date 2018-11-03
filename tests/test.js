259995c1f7569c529ec7da085f80e01636c9ae23
levi <levi2p@gmail.com>
2018-11-04 00:16:06 +0200

    Removed commented out code.

--- tests/test.js
+++ tests/test.js
@@ -54,16 +54,3 @@ function delay(timeMillis) {
 		setTimeout(resolve, timeMillis);
 	});
 }
-
-// const {suite, test} = require('../dist/index');
-//
-// suite('Array', () => {
-// 	suite('indexOf', () => {
-// 		test('success', () => {
-// 			// console.log('Inside a success test!');
-// 		});
-// 		test('failure', () => {
-// 			throw new Error('Assertion failure.');
-// 		});
-// 	});
-// });
